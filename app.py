310b4ed4c7c7a751cc5625ac932b8d76911ccd04
DanielZhong24 <danielzhong0618@gmail.com>
2024-06-04 19:10:24 -0400

    gitnore

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('view/index.html')
+    return render_template("view/index.html")
 
 @app.route('/music')
 def music():
