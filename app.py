d613b3e1ec4854396dffaec6cd40f6a8979fd73a
brightoe <brighton.truong@hotmail.com>
2024-06-04 19:09:55 -0400

    website

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask 
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='view')
 
 @app.route('/')
 def home():
-    return render_template('view/index.html')
+    return render_template('index.html')
 
 @app.route('/music')
 def music():
     return '<h1>This is a page for music !!!!!</h1>'
 
-#main loop
+@app.route('/random')
+def random_page():
+    return render_template('random.html')
+
+# Main loop
 if __name__ == '__main__':
     app.run(debug=True)
