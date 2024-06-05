abcaf381547ab006389f0cc8cc9bc00e5b507cff
DanielZhong24 <danielzhong0618@gmail.com>
2024-06-04 23:02:09 -0400

    Update StudentController.py

--- app/controller/StudentController.py
+++ app/controller/StudentController.py
@@ -5,8 +5,8 @@ from ..model.StudentModel import MusicStudent
 
 student_bp = Blueprint('student_bp', __name__)
 
-@student_bp.route('/students')
+@student_bp.route('/student')
 def students():
-    # students = MusicStudent.query.all()
-    students = MusicStudent.query.filter_by(student_lastName = "Truong").all()
+    students = MusicStudent.query.all()
+    # students = MusicStudent.query.filter_by(student_lastName = "Truong").all()
     return render_template('student/student.html', students=students)
