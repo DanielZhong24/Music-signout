0052c59469c5af70120153c54681fa14b546e0b2
brighton473 <125299761+brighton473@users.noreply.github.com>
2024-06-06 12:27:31 -0400

    student

--- app/controller/StudentController.py
+++ app/controller/StudentController.py
@@ -1,29 +1,29 @@
 # app/controller/StudentController.py
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from ..model.StudentModel import MusicStudent
-
+from app import db
 student_bp = Blueprint('student_bp', __name__)
 
-@student_bp.route('/student')
+@student_bp.route('/student',methods = ['GET'])
 def students():
     students = MusicStudent.query.all()
     # students = MusicStudent.query.filter_by(student_lastName = "Truong").all()
     return render_template('student/student.html', students=students)
 
 @student_bp.route('/addStudent', methods=['GET', 'POST'])
-def add_instrument():
+def add_student():
     if request.method == 'POST':
         student_firstName = request.form['student_firstName']
         student_lastName = request.form['student_lastName']
         student_id = request.form['student_id']
         grade = request.form['grade']
         
-        existing_student = students.query.filter_by(students=students,student_id=student_id).first()
+        existing_student = MusicStudent.query.filter_by(students=students,student_id=student_id).first()
         if existing_student:
             flash('Student with the same student number already exists!', 'danger')
             return redirect(url_for('student_bp.add_student'))
-        new_student = Student(student_firstName, student_lastName, student_id, grade)
+        new_student = MusicStudent(student_firstName, student_lastName, student_id, grade)
         db.session.add(new_student)
         db.session.commit()
 
--- app/model/StudentModel.py
+++ app/model/StudentModel.py
@@ -10,3 +10,11 @@ class MusicStudent(db.Model):
     student_id = db.Column(db.String(80), nullable=False)
     grade = db.Column(db.Integer, nullable=False)
     #make contructor
+
+    __table_args__ = (db.UniqueConstraint('student','student_id', name="unique_student_id"),)
+    def __init__(self,student_firstName,student_lastName,student_id,grade):
+        self.student_firstName = student_firstName
+        self.student_lastName = student_lastName
+        self.student_id = student_id
+        self.grade = grade
+     
