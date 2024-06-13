e80db1ec9d67a8fd62e2f15c4bdbd1497dbd259e
brighton473 <125299761+brighton473@users.noreply.github.com>
2024-06-13 11:45:45 -0400

    selection working

--- app/model/StudentModel.py
+++ app/model/StudentModel.py
@@ -1,16 +1,14 @@
-# app/model/StudentModel.py
-
-from .. import db
+from app import db
 
 class MusicStudent(db.Model):
     __tablename__ = 'music_students'
+
     id = db.Column(db.Integer, primary_key=True)
-    student_firstName = db.Column(db.String(80), nullable=False)
-    student_lastName = db.Column(db.String(80), nullable=False)
-    student_id = db.Column(db.String(80), nullable=False)
-    grade = db.Column(db.Integer, nullable=False)
-    
-    __table_args__ = (db.UniqueConstraint('student_firstName', 'student_lastName', 'student_id', name="unique_student_id"),)
+    student_firstName = db.Column(db.String(50))
+    student_lastName = db.Column(db.String(50))
+    student_id = db.Column(db.String(20), unique=True)
+    grade = db.Column(db.String(10))
+
     def __init__(self, student_firstName, student_lastName, student_id, grade):
         self.student_firstName = student_firstName
         self.student_lastName = student_lastName
